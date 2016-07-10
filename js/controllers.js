c1a1c0e289151b6387c29c96859b2b3a6ef66b55
janepalamine <mpalamine26@gmail.com>
2016-07-10 12:55:30 +0800

    Add files via upload

---
+++ js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('chatAppCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('pageCtrl', function($scope) {
+
+})
+ 
---
+++ js/routes.js
@@ -0,0 +1,41 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('chatApp', {
+    url: '/page',
+    templateUrl: 'templates/chatApp.html',
+    controller: 'chatAppCtrl'
+  })
+
+  .state('signup', {
+    url: '/page3',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('login', {
+    url: '/page4',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('page', {
+    url: '/page5',
+    templateUrl: 'templates/page.html',
+    controller: 'pageCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page')
+
+  
+
+});
